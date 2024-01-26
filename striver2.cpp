6e5f37ba192f5d13899843b962debfb4b2183ad7
Gaurav Kunwar <69753962+gauravkunwarr@users.noreply.github.com>
2024-01-26 22:41:06 +0530

    Add files via upload

---
+++ striver2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    cout<<"Enter two Number "<<endl;
+    cin>>x;
+    cin>>y;
+
+    cout<<"Value of X and Y = "<<x<<"   "<<y;
+    
+    return 0;
+}
